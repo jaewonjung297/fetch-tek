b38c8b7e1081a685cb400603578ea1517f473633
Jaewon Jung <jaewon.j.1@gmail.com>
2022-11-26 20:30:24 -0500

    added unique id enpoint in server.js

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 const data = {
     products: [
         {
+            _id: '1',
             name: 'iPhone 14 Pro Max',
             slug: 'iphone-14-pm',
             category: 'phones',
@@ -13,6 +14,7 @@ const data = {
             description: 'The iPhone 14 Pro 5G Dual SIM comes with a 6.1 inch touchscreen with 460PPI. It packs a 48-megapixel pro camera with main ultra wide system and a 12-megapixel selfie-camera with TrueDepth with autofocus. This is all powered by the Apple A16 Bionic (4 nm) chipset and 6GB of RAM.'
         },
         {
+            _id: '2',
             name: 'iPhone 11',
             slug: 'iphone-11',
             category: 'phones',
@@ -23,7 +25,9 @@ const data = {
             rating: 4.4,
             numReviews: 13,
             description: 'The iPhone 11 is a smartphone designed, developed, and marketed by Apple Inc. It is the 13th generation of iPhone, succeeding the iPhone XR, and was unveiled on September 10, 2019 alongside the iPhone 11 Pro at the Steve Jobs Theater in Apple Park, Cupertino, by Apple CEO Tim Cook. Preorders began on September 13, 2019, and the phone was officially released on September 20, 2019, one day after the official public release of iOS 13.'
-        },        {
+        },        
+        {
+            _id: '3',
             name: 'iPhone XR',
             slug: 'iphone-xr',
             category: 'phones',
@@ -34,13 +38,15 @@ const data = {
             rating: 4.8,
             numReviews: 10,
             description: 'The iPhone 14 Pro 5G Dual SIM comes with a 6.1 inch touchscreen with 460PPI. It packs a 48-megapixel pro camera with main ultra wide system and a 12-megapixel selfie-camera with TrueDepth with autofocus. This is all powered by the Apple A16 Bionic (4 nm) chipset and 6GB of RAM.'
-        },        {
+        },        
+        {
+            _id: '4',
             name: 'Pixel 6 pro',
             slug: 'pixel-6-p',
             category: 'phones',
             image: '/images/Pixel_6_Pro.webp',
             price: 1099.99,
-            countInStock: 10,
+            countInStock: 12,
             brand: 'Apple',
             rating: 4.8,
             numReviews: 10,
